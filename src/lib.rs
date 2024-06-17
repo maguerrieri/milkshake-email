94659fef3ff93ff7bc596406024d20b98190c5df
Mario Guerrieri <mario@guerrieri.codes>
2024-06-16 17:48:29 -0700

    Do an RPC???

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,17 @@
+extern crate console_error_panic_hook;
+
+use wasm_bindgen::prelude::wasm_bindgen;
+// use http::Response;
 use worker::*;
 
-#[event(fetch)]
-async fn main(req: Request, env: Env, ctx: Context) -> Result<Response> {
-    Response::ok("Hello, World!")
+#[wasm_bindgen]
+pub async fn add(a: u32, b: u32) -> u32 {
+    console_error_panic_hook::set_once();
+    a + b
 }
+
+// #[event(fetch)]
+// async fn fetch(_req: HttpRequest, _env: Env, _ctx: Context) -> Result<http::Response<String>> {
+//     Ok(Response::builder()
+//         .body("test".to_string())?)
+// }
