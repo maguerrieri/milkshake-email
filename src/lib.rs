d5369cf170aad09a22d3aa9962947a1e6bc771d7
Mario Guerrieri <mario@guerrieri.codes>
2024-06-19 17:13:47 -0700

    Clean up

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 extern crate console_error_panic_hook;
 
 use wasm_bindgen::prelude::wasm_bindgen;
-// use http::Response;
 use worker::*;
 
 #[wasm_bindgen]
@@ -9,9 +8,3 @@ pub async fn add(a: u32, b: u32) -> u32 {
     console_error_panic_hook::set_once();
     a + b
 }
-
-// #[event(fetch)]
-// async fn fetch(_req: HttpRequest, _env: Env, _ctx: Context) -> Result<http::Response<String>> {
-//     Ok(Response::builder()
-//         .body("test".to_string())?)
-// }
